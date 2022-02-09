7b81fd48f7a8bd0d9faccfd986ab36891cee7be2
DarDog <samediacc666@gmial.com>
2022-02-09 18:16:28 +1100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,336 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="page">
+    <div class="container">
+      <header id="masthead" class="site-header">
+        <div class="site-branding">
+          <h1 class="site-title">
+            <a href="index.html" rel="home">Moschino</a>
+          </h1>
+          <h2 class="site-description">Minimalist Portfolio HTML Template</h2>
+        </div>
+        <nav id="site-navigation" class="main-navigation">
+          <button class="menu-toggle">Menu</button>
+          <a class="skip-link screen-reader-text" href="#content"
+            >Skip to content</a
+          >
+          <div class="menu-menu-1-container">
+            <ul id="menu-menu-1" class="menu">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="about.html">About</a></li>
+              <li><a href="shop.html">Shop</a></li>
+              <li><a href="blog.html">Blog</a></li>
+              <li><a href="elements.html">Elements</a></li>
+              <li>
+                <a href="#">Pages</a>
+                <ul class="sub-menu">
+                  <li><a href="portfolio-item.html">Portfolio Item</a></li>
+                  <li><a href="blog-single.html">Blog Article</a></li>
+                  <li><a href="shop-single.html">Shop Item</a></li>
+                  <li>
+                    <a href="portfolio-category.html">Portfolio Category</a>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="contact.html">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <!-- #masthead -->
+      <div id="content" class="site-content">
+        <div id="primary" class="content-area column full">
+          <main id="main" class="site-main" role="main">
+            <div class="grid portfoliogrid posts">
+              <article>
+                <header class="entry-header">
+                  <h1 class="entry-title">
+                    <a href="blog-single.html" rel="bookmark"
+                      >Somewhere in time</a
+                    >
+                  </h1>
+                  <div class="entry-meta">
+                    <span class="posted-on"
+                      ><time class="entry-date published"
+                        >April 12, 2016</time
+                      ></span
+                    >
+                    <span class="comments-link"
+                      ><a href="#">Leave a comment</a></span
+                    >
+                  </div>
+                  <div class="entry-thumbnail">
+                    <img
+                      src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/10/2015/09/30160348/sep4.jpg"
+                      alt=""
+                    />
+                  </div>
+                </header>
+                <div class="entry-summary">
+                  <p>
+                    When Mary Lennox was sent to Manor to live with her uncle
+                    everybody said she was the most disagreeablelooking child
+                    ever seen. It was true, too. She had a little thin face and
+                    a little thin body, thin light hair and a sour expression.
+                    Her hair was yellow, and her face was yellow because she
+                    <a class="more-link" href="blog-single.html">Read more</a>
+                  </p>
+                </div>
+                <footer class="entry-footer">
+                  <span class="cat-links">
+                    Posted in <a href="#" rel="category tag">audio</a>,
+                    <a href="#" rel="category tag">embed</a>,
+                    <a href="#" rel="category tag">media</a>
+                  </span>
+                </footer>
+              </article>
+
+              <article>
+                <header class="entry-header">
+                  <h1 class="entry-title">
+                    <a href="blog-single.html" rel="bookmark"
+                      >Thanks for watching</a
+                    >
+                  </h1>
+                  <div class="entry-meta">
+                    <span class="posted-on"
+                      ><time class="entry-date published"
+                        >April 11, 2016</time
+                      ></span
+                    >
+                    <span class="comments-link"><a href="#">1 Comment</a></span>
+                  </div>
+                  <div class="entry-thumbnail">
+                    <img
+                      src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/10/2015/09/30162427/sep2.jpg"
+                      alt=""
+                    />
+                  </div>
+                </header>
+                <div class="entry-summary">
+                  <p>
+                    When Mary Lennox was sent to Manor to live with her uncle
+                    everybody said she was the most disagreeablelooking child
+                    ever seen. It was true, too. She had a little thin face and
+                    a little thin body, thin light hair and a sour expression.
+                    Her hair was yellow, and her face was yellow because she
+                    <a class="more-link" href="blog-single.html">Read more</a>
+                  </p>
+                </div>
+                <footer class="entry-footer">
+                  <span class="cat-links">
+                    Posted in <a href="#" rel="category tag">audio</a>,
+                    <a href="#" rel="category tag">embed</a>,
+                    <a href="#" rel="category tag">media</a>
+                  </span>
+                </footer>
+              </article>
+
+              <article>
+                <header class="entry-header">
+                  <h1 class="entry-title">
+                    <a href="blog-single.html" rel="bookmark">Dreaming Away</a>
+                  </h1>
+                  <div class="entry-meta">
+                    <span class="posted-on"
+                      ><time class="entry-date published"
+                        >April 10, 2016</time
+                      ></span
+                    >
+                    <span class="comments-link"
+                      ><a href="#">Leave a comment</a></span
+                    >
+                  </div>
+                  <div class="entry-thumbnail">
+                    <img
+                      src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/10/2015/09/15211639/p3.jpg"
+                      alt=""
+                    />
+                  </div>
+                </header>
+                <div class="entry-summary">
+                  <p>
+                    When Mary Lennox was sent to Manor to live with her uncle
+                    everybody said she was the most disagreeablelooking child
+                    ever seen. It was true, too. She had a little thin face and
+                    a little thin body, thin light hair and a sour expression.
+                    Her hair was yellow, and her face was yellow because she
+                    <a class="more-link" href="blog-single.html">Read more</a>
+                  </p>
+                </div>
+                <footer class="entry-footer">
+                  <span class="cat-links">
+                    Posted in <a href="#" rel="category tag">audio</a>,
+                    <a href="#" rel="category tag">embed</a>,
+                    <a href="#" rel="category tag">media</a>
+                  </span>
+                </footer>
+              </article>
+
+              <article>
+                <header class="entry-header">
+                  <h1 class="entry-title">
+                    <a href="blog-single.html" rel="bookmark">Sunny Days</a>
+                  </h1>
+                  <div class="entry-meta">
+                    <span class="posted-on"
+                      ><time class="entry-date published"
+                        >April 9, 2016</time
+                      ></span
+                    >
+                    <span class="comments-link"
+                      ><a href="#">2 Comments</a></span
+                    >
+                  </div>
+                  <div class="entry-thumbnail">
+                    <img
+                      src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/10/2015/09/15222855/sep1.jpg"
+                      alt=""
+                    />
+                  </div>
+                </header>
+                <div class="entry-summary">
+                  <p>
+                    When Mary Lennox was sent to Manor to live with her uncle
+                    everybody said she was the most disagreeablelooking child
+                    ever seen. It was true, too. She had a little thin face and
+                    a little thin body, thin light hair and a sour expression.
+                    Her hair was yellow, and her face was yellow because she
+                    <a class="more-link" href="blog-single.html">Read more</a>
+                  </p>
+                </div>
+                <footer class="entry-footer">
+                  <span class="cat-links">
+                    Posted in <a href="#" rel="category tag">audio</a>,
+                    <a href="#" rel="category tag">embed</a>,
+                    <a href="#" rel="category tag">media</a>
+                  </span>
+                </footer>
+              </article>
+
+              <article>
+                <header class="entry-header">
+                  <h1 class="entry-title">
+                    <a href="blog-single.html" rel="bookmark">Holiday Travel</a>
+                  </h1>
+                  <div class="entry-meta">
+                    <span class="posted-on"
+                      ><time class="entry-date published"
+                        >April 6, 2016</time
+                      ></span
+                    >
+                    <span class="comments-link"
+                      ><a href="#">Leave a comment</a></span
+                    >
+                  </div>
+                  <div class="entry-thumbnail">
+                    <img
+                      src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/10/2015/09/15223245/p6.jpg"
+                      alt=""
+                    />
+                  </div>
+                </header>
+                <div class="entry-summary">
+                  <p>
+                    When Mary Lennox was sent to Manor to live with her uncle
+                    everybody said she was the most disagreeablelooking child
+                    ever seen. It was true, too. She had a little thin face and
+                    a little thin body, thin light hair and a sour expression.
+                    Her hair was yellow, and her face was yellow because she
+                    <a class="more-link" href="blog-single.html">Read more</a>
+                  </p>
+                </div>
+                <footer class="entry-footer">
+                  <span class="cat-links">
+                    Posted in <a href="#" rel="category tag">audio</a>,
+                    <a href="#" rel="category tag">embed</a>,
+                    <a href="#" rel="category tag">media</a>
+                  </span>
+                </footer>
+              </article>
+
+              <article>
+                <header class="entry-header">
+                  <h1 class="entry-title">
+                    <a href="blog-single.html" rel="bookmark">Driving Summer</a>
+                  </h1>
+                  <div class="entry-meta">
+                    <span class="posted-on"
+                      ><time class="entry-date published"
+                        >April 7, 2016</time
+                      ></span
+                    >
+                    <span class="comments-link"
+                      ><a href="#">3 Comments</a></span
+                    >
+                  </div>
+                  <div class="entry-thumbnail">
+                    <img
+                      src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/10/2015/09/18160911/4.jpg"
+                      alt=""
+                    />
+                  </div>
+                </header>
+                <div class="entry-summary">
+                  <p>
+                    When Mary Lennox was sent to Manor to live with her uncle
+                    everybody said she was the most disagreeablelooking child
+                    ever seen. It was true, too. She had a little thin face and
+                    a little thin body, thin light hair and a sour expression.
+                    Her hair was yellow, and her face was yellow because she
+                    <a class="more-link" href="blog-single.html">Read more</a>
+                  </p>
+                </div>
+                <footer class="entry-footer">
+                  <span class="cat-links">
+                    Posted in <a href="#" rel="category tag">audio</a>,
+                    <a href="#" rel="category tag">embed</a>,
+                    <a href="#" rel="category tag">media</a>
+                  </span>
+                </footer>
+              </article>
+            </div>
+            <div class="clearfix"></div>
+            <nav class="pagination"></nav>
+          </main>
+          <!-- #main -->
+        </div>
+      </div>
+      <!-- #content -->
+    </div>
+    <!-- .container -->
+    <footer id="colophon" class="site-footer">
+      <div class="container">
+        <div class="site-info">
+          <h1
+            style="
+              font-family: 'Herr Von Muellerhoff';
+              color: #ccc;
+              font-weight: 300;
+              text-align: center;
+              margin-bottom: 0;
+              margin-top: 0;
+              line-height: 1.4;
+              font-size: 46px;
+            "
+          >
+            Moschino
+          </h1>
+          <a target="blank" href="https://www.wowthemes.net/"
+            >&copy; Moschino - Free HTML Template by WowThemes.net</a
+          >
+        </div>
+      </div>
+    </footer>
+    <a href="#top" class="smoothup" title="Back to top"
+      ><span class="genericon genericon-collapse"></span
+    ></a>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./styles/style.css" />
