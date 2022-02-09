274bb6b1711af6acdb64a7262971a1e6016a959d
DarDog <samediacc666@gmial.com>
2022-02-09 18:34:53 +1100

    Feat add Footer component

--- src/App.vue
+++ src/App.vue
@@ -263,41 +263,16 @@
       </div>
       <!-- #content -->
     </div>
-    <!-- .container -->
-    <footer id="colophon" class="site-footer">
-      <div class="container">
-        <div class="site-info">
-          <h1
-            style="
-              font-family: 'Herr Von Muellerhoff';
-              color: #ccc;
-              font-weight: 300;
-              text-align: center;
-              margin-bottom: 0;
-              margin-top: 0;
-              line-height: 1.4;
-              font-size: 46px;
-            "
-          >
-            Moschino
-          </h1>
-          <a target="blank" href="https://www.wowthemes.net/"
-            >&copy; Moschino - Free HTML Template by WowThemes.net</a
-          >
-        </div>
-      </div>
-    </footer>
-    <a href="#top" class="smoothup" title="Back to top"
-      ><span class="genericon genericon-collapse"></span
-    ></a>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 export default {
   name: "App",
-  components: { Header },
+  components: { Footer, Header },
 };
 </script>
 
